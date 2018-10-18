8fd443bba77047bbcf114e6b944916b1917a8c08
George Warner <warner_g_3@yahoo.com>
2018-10-18 13:00:55 -0500

    Initial Commit, EmployeeSelectListViewModel added

--- DevExpressTesting/App_Start/BundleConfig.cs
+++ DevExpressTesting/App_Start/BundleConfig.cs
@@ -1,4 +1,4 @@
-﻿using System.Web;
+using System.Web;
 using System.Web.Optimization;
 
 namespace DevExpressTesting
@@ -9,7 +9,7 @@ namespace DevExpressTesting
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.10.2.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ DevExpressTesting/ViewModels/EmployeeSelectListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevExpressTesting.ViewModels
+{
+    public class EmployeeSelectListViewModel
+    {
+        public string EmployeeNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string HomeDepartment { get; set; }
+    }
+}
